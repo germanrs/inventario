7e84605268162a89f031b4739a39582e64a6ed79
German R <gars1029@gmail.com>
2019-04-01 15:53:53 -0400

    Agregar contador en las listas

--- laptop/admin.py
+++ laptop/admin.py
@@ -18,10 +18,11 @@ admin.site.register(Incidente)
 
 admin.site.register(Componente)
 
-class LaptopAdmin(admin.ModelAdmin):
-    list_display = ('id', 'serie', 'uuid', 'modelo')
-admin.site.register(Laptop, LaptopAdmin)
+#class LaptopAdmin(admin.ModelAdmin):
+#    list_display = ('id', 'serie', 'uuid', 'modelo')
+#admin.site.register(Laptop, LaptopAdmin)
 
-#@admin.register(Laptop)
-#class LaptopAdmin(ImportExportModelAdmin):
-#    pass
+@admin.register(Laptop)
+class LaptopAdmin(ImportExportModelAdmin):
+	list_display = ('id', 'serie', 'uuid', 'modelo')
+	pass
